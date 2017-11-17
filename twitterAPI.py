f56bc6bab788aa682d5541936aa7d342c3f95d0f
Haoran Zhang <g090617@gmail.com>
2017-11-17 02:45:10 -0800

    add daemon

--- twitterAPI.py
+++ twitterAPI.py
@@ -2,8 +2,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 import json
-
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import datetime
 
 consumer_key = "v7PBrhQmnQAnMNkfFYwKRZZ3t"
 consumer_secret = "3HO5D8WJqszD6jXvUmLMXQk44zu8vXiQM2eo3QikYcrMC4IkDe"
@@ -12,7 +11,7 @@ access_token_secret = "c9iT2FJlfe6vv01Tn8xQJQIZkyXEzUfJJaky8J4uPRBjo"
 
 
 class stdOutListener(StreamListener):
-    lineCount = 0
+
     def on_data(self, data):
         tweet = json.loads(data)
 
@@ -20,48 +19,41 @@ class stdOutListener(StreamListener):
             if not tweet['retweeted'] and \
                             'RT @' not in tweet['text'] and \
                             tweet['lang'] == "en":
-                print(tweet['text'], '\n')
-                print(self.lineCount)
-                print("=============================")
+                # print(tweet['text'], '\n')
+                # print("=============================")
 
-                sentences = [tweet['text']]
-                analyzer = SentimentIntensityAnalyzer()
-                for sentence in sentences:
-                    vs = analyzer.polarity_scores(sentence)
-                    print("{:-<65} {}".format(sentence, str(vs)))
-
-                with open('data.json', 'a') as f:
+                fileString = 'data/' + str(datetime.datetime.now().date()) + '-' +\
+                             str(datetime.datetime.now().hour) + '.json'
+                with open(fileString, 'a') as f:
                     f.write(tweet['text'] + '\n')
                     # f.write(data)
-                    self.lineCount += 1
-                    if self.lineCount == 200:
-                        return False
                     return True
 
     def on_error(self, status):
         print('status', status)
 
 if __name__ == '__main__':
+
     l = stdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     
     stream = Stream(auth, l)
-    stream.filter(track=["alphabet inc",
-                         "artificial intelligence",
-                         "machine learning",
-                         "Google cloud platform",
-                         "computer vision",
-                         "deep learning"], async=True)
+    stream.filter(track=["microsoft",
+                         "Microsoft",
+                         "Azure",
+                         "XBox",
+                         "windows",
+                         "Windows",
+                         "Bing",
+                         "bing",
+                         "surface",
+                         "Visual Studio",
+                         "DirectX",
+                         "SQL"], async=True)
 
 
 
-    # sentences = ["UCLA CSE sucks",
-    #             "UCLA is the best!!"]
-    # analyzer = SentimentIntensityAnalyzer()
-    # for sentence in sentences:
-    #     vs = analyzer.polarity_scores(sentence)
-    #     print("{:-<65} {}".format(sentence, str(vs)))
 
 
 
